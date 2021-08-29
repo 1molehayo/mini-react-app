c9e3328730a23bc68760ef4c4f36eba45f948f7b
Olusegun Omilabu <omilabuolusegun@gmail.com>
2021-08-29 02:29:43 +0100

    setup react app

--- api_server.js
+++ api_server.js
@@ -1,16 +1,20 @@
-var fs = require('fs');
-var http = require('http');
+var fs = require("fs");
+var http = require("http");
 
-http.createServer(function (req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+http
+  .createServer(function (req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
-  fs.readFile(__dirname + req.url, function (err,data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
-}).listen(4000);
+    fs.readFile(__dirname + req.url, function (err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  })
+  .listen(4000);
+
+console.log("Server running at http://localhost:4000");
